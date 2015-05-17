34a52927a72e486a50d095e0039ffdf75027c958
fbidu <bidu.pub@gmail.com>
2015-05-17 04:50:45 -0300

    Cleaning

--- app/routes.php
+++ app/routes.php
@@ -1,79 +1,77 @@
 <?php
 
-
-Route::get('/', function()
+Route::get('/', function ()
 {
+
+	print("Welcome!");
+	
 	$login = Facebook::getLoginUrl(['email', 'user_likes', 'user_posts']);
 
 	return '<a href="' . $login . '">Log in with Facebook!</a>';
 });
 
 
-Route::get('/facebook/login', function()
+Route::get('/facebook/login', function ()
 {
-	$token = Facebook::getTokenFromRedirect();
+	try
+	{
+		$token = Facebook::getTokenFromRedirect();
 
-	dd($token);
-    try
-    {
-        $token = Facebook::getTokenFromRedirect();
+		if (!$token)
+		{
+			return "Error obtaining token!";
+		}
+	} catch (FacebookQueryBuilderException $e)
+	{
+		// Facebook query builder error!
+		return $e->getPrevious()->getMessage();
+	}
 
-        if (!$token)
-        {
-            return "Error obtaining token!";
-        }
-    }
-    catch (FacebookQueryBuilderException $e)
-    {
-        // Facebook query builder error!
-        return $e->getPrevious()->getMessage();
-    }
+	// If the token is short lived, we'll extend it
+	// Ref: https://developers.facebook.com/docs/facebook-login/access-tokens#extending
+	if (!$token->isLongLived())
+	{
+		try
+		{
+			$token = $token->extend();
+		} catch (FacebookQueryBuilderException $e)
+		{
+			return Redirect::to('/')->with('error', $e->getPrevious()->getMessage());
+		}
+	}
 
+	// Setting the access token
+	Facebook::setAccessToken($token);
 
-    // If the token is short lived, we'll extend it
-    // Ref: https://developers.facebook.com/docs/facebook-login/access-tokens#extending
-    if (!$token->isLongLived())
-    {
-        try
-        {
-            $token = $token->extend();
-        }
-        catch (FacebookQueryBuilderException $e)
-        {
-            return Redirect::to('/')->with('error', $e->getPrevious()->getMessage());
-        }
-    }
+	Session::put('facebook_access_token', (string) $token);
 
-    // Setting the access token
-    Facebook::setAccessToken($token);
-    Session::put('facebook_access_token', (string) $token);
+	// Getting some basic user info
+	try
+	{
+		$facebook_user = Facebook::object('me')->fields('id', 'name')->get();
+	} catch (FacebookQueryBuilderException $e)
+	{
+		return Redirect::to('/')->with('error', $e->getPrevious()->getMessage());
+	}
 
-    // Getting some basic user info
-    try
-    {
-        $facebook_user = Facebook::object('me')->fields('id','name')->get();
-    }
-    catch (FacebookQueryBuilderException $e)
-    {
-        return Redirect::to('/')->with('error', $e->getPrevious()->getMessage());
-    }
+    $user = User::createOrUpdateFacebookObject($facebook_user);
 
-//    $user = User::createOrUpdateFacebookObject($facebook_user);
+    Facebook::auth()->login($user);
 
-//    Facebook::auth()->login($user);
-
-    return Redirect::to("/posts/10");
+	return Redirect::to("/posts/10");
 });
 
-Route::get('/posts/{count}', function($count)
+Route::get('/posts/{count}', function ($count)
 {
-    $token = Session::get('facebook_access_token');
-    Facebook::setAccessToken($token);
+	$token = Session::get('facebook_access_token');
+	Facebook::setAccessToken($token);
 
-	try {
-		$posts = Facebook::object('me/feed')->with(['with' => 'location',
-		'limit' => $count])->get();
-	} catch (\SammyK\FacebookQueryBuilder\FacebookQueryBuilderException $e) {
+	try
+	{
+		$posts = Facebook::object('me/feed')->with(['with'  => 'location',
+													'limit' => $count])->get();
+	} catch (\SammyK\FacebookQueryBuilder\FacebookQueryBuilderException $e)
+	{
 		dd($e->getPrevious()->getMessage());
 	}
 
@@ -81,22 +79,25 @@ Route::get('/posts/{count}', function($count)
 	{
 		if ($post->has('message'))
 		{
-			print $post['message'].'<br>';
+			print $post['message'] . '<br>';
 			$place = Facebook::object($post['place']['id'])->get();
-			print $post['place']['name'].' - '.$place['category'];
+			print $post['place']['name'] . ' - ' . $place['category'];
 			print "<br>";
 		}
 	}
 
 });
 
-Route::get('/facebook/callback', function() {
+Route::get('/facebook/callback', function ()
+{
 	$fb = App::make('SammyK\LaravelFacebookSdk\LaravelFacebookSdk');
-	try {
+	try
+	{
 		$token = $fb
 			->getRedirectLoginHelper()
 			->getAccessToken();
-	} catch (FacebookSDKException $e) {
+	} catch (FacebookSDKException $e)
+	{
 		// Failed to obtain access token
 		dd($e->getMessage());
 	}
