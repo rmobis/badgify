2d71da0dec3116e1b58224887fc60cfbcd16e03b
fbidu <bidu.pub@gmail.com>
2015-05-17 02:37:42 -0300

    trying to set the call

--- app/routes.php
+++ app/routes.php
@@ -3,7 +3,7 @@
 
 Route::get('/', function()
 {
-	$login = Facebook::getLoginUrl(['email', 'user_likes', 'user_posts'], 'https://badgify.rmob.is/facebook/login/');
+	$login = Facebook::getLoginUrl(['email', 'user_likes', 'user_posts'], 'https://badgify.rmob.is/facebook/login');
 
 	return '<a href="' . $login . '">Log in with Facebook!</a>';
 });
@@ -23,7 +23,7 @@ Route::get('/', function()
 
 Route::post('/', function()
 {
-	$login = Facebook::getLoginUrl(['email', 'user_likes', 'user_posts'], 'https://badgify.rmob.is/facebook/login/');
+	$login = Facebook::getLoginUrl(['email', 'user_likes', 'user_posts'], 'https://badgify.rmob.is/facebook/login');
 
 	return '<a href="' . $login . '">Log in with Facebook!</a>';
 
