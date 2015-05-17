eef44199c4182d9abc4e82ef36975b65d47979d1
fbidu <bidu.pub@gmail.com>
2015-05-17 02:42:16 -0300

    Fixing the app

--- app/routes.php
+++ app/routes.php
@@ -23,7 +23,7 @@ Route::get('/', function()
 
 Route::post('/', function()
 {
-	$login = Facebook::getLoginUrl(['email', 'user_likes', 'user_posts']);
+	$login = Facebook::getLoginUrl(['email', 'user_likes', 'user_posts'], url('https://badgify.rmob.is/facebook/login'));
 
 	return '<a href="' . $login . '">Log in with Facebook!</a>';
 
