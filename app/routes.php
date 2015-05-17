d2f19ed674fa8b3ff4a655aaa1e6e9b1e93afb5e
fbidu <bidu.pub@gmail.com>
2015-05-17 00:42:01 -0300

    Using the Login view

--- app/routes.php
+++ app/routes.php
@@ -8,7 +8,7 @@ Route::get('/', function()
 
 Route::post('/', function()
 {
-	return Redirect::to('https://badgify.rmob.is/login');
+	return View::make('login');
 });
 
 Route::get('/login', function()
